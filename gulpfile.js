9f25c0bedabe1203fbf297db99767db8c3044316
VladProfitkit <vlad@profitkit.ru>
2022-07-11 16:29:25 +0300

    Init

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+
+gulp.task("css", function () {
+  return gulp.src("source/css/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    //.pipe(sass({includePaths: require("node-normalize-scss").includePaths}))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("js", function() {
+  return gulp.src("source/js/script.js")
+    .pipe(gulp.dest("build/js"));
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/css/**/*.{scss,sass}", gulp.series("css", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("vendor", function() {
+  return gulp.src([
+    "source/vendor/**/*.{css,scss,sass,map,js}",
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", gulp.series("clean", "vendor", "css", "js","images", "html"));
+gulp.task("start", gulp.series("build", "server"));
