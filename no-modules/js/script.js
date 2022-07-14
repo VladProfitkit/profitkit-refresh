4d09841e7a36b86586481993735ec2f6b710b6f8
VladProfitkit <vlad@profitkit.ru>
2022-07-14 19:14:38 +0300

    Продолжение верстки

--- no-modules/js/script.js
+++ no-modules/js/script.js
@@ -29,12 +29,39 @@ $(document).ready(function() {
             }
         }
     });
+
+    //слайдер портфолио:
+    let protfolioSlider = $('.portfolio__slides');
+
+    if (protfolioSlider.length) {
+        protfolioSlider.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide) {
+            let currentSlideSpan = protfolioSlider.parent().find('.portfolio__nav-counter-current'),
+                totalSlidesSpan = protfolioSlider.parent().find('.portfolio__nav-counter-total'),
+                currentSlideNumber = (currentSlide ? currentSlide : 0) + 1,
+                totalSlidesNumber = slick.slideCount;
+
+            currentSlideSpan.text(currentSlideNumber);
+            totalSlidesSpan.text(totalSlidesNumber);
+        });
+
+        protfolioSlider.slick({
+            speed: 200,
+            infinite: false,
+            variableWidth: true,
+            swipeToSlide: true,
+            touchThreshold: 200,
+            prevArrow: protfolioSlider.parent().find('.portfolio__nav-btn--prev'),
+            nextArrow: protfolioSlider.parent().find('.portfolio__nav-btn--next'),
+        });
+    }
 });
 
 $(window).on('load resize', function() {
-    let bannerZoneDescription = $('.banner-zone__description--service');
+    let bannerZoneDescription = $('.banner-zone__description--service'),
+        portfolioSlideImg = $('.portfolio__slide-img');
 
     if ($(window).width() <= 767) {
+        //текст под баннером на странице услуги:
         bannerZoneDescription.readmore({
           speed: 200,
           collapsedHeight: 50,
@@ -42,7 +69,20 @@ $(window).on('load resize', function() {
           moreLink: '<a class="banner-zone__description-expand closed" href="javascipt:;">Читать полностью</a>',
           lessLink: '<a class="banner-zone__description-expand open" href="javascipt:;">Свернуть</a>',
         });
+
+        //автоподмена картинок в портфолио:
+        portfolioSlideImg.each(function() {
+          let portfolioSlideImgMobSrc = $(this).data('mobile-src');
+          $(this).attr('src', portfolioSlideImgMobSrc);
+        });
     } else {
+        //текст под баннером на странице услуги:
         bannerZoneDescription.readmore('destroy');
+
+        //автоподмена картинок в портфолио:
+        portfolioSlideImg.each(function() {
+          let portfolioSlideImgDeskSrc = $(this).data('desktop-src');
+          $(this).attr('src', portfolioSlideImgDeskSrc);
+        });
     }
 });
